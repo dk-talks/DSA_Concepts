703dbc52f3bc0b64dea03b238895c6fa7864f74a
Dinesh Sharma <65244911+dk-talks@users.noreply.github.com>
2023-06-17 22:10:12 +0530

    Update NQueen.java

--- Backtracking/NQueen.java
+++ Backtracking/NQueen.java
@@ -27,11 +27,41 @@ class NQueen {
         
         for(int col = 0; col<board.length; col++) {
 
-          // this code will give all permutations where Queen can be placed without thinking about attack
-            board[row][col] = 'Q';
-            nQueens(board, row+1);
-            board[row][col] = 'x';
+            // check first is it safe to put the queen at this position
+            if(isSafe(board, row, col)) {
+                // this code will give all permutations where Queen can be placed without thinking about attack
+                board[row][col] = 'Q';
+                nQueens(board, row + 1);
+                board[row][col] = 'x';
+            }
         }
+    }
+
+    public static boolean isSafe(char[][] board, int row, int col) {
+        // we need to check three paths from where upper queens can attack the current queen.
+
+        // check vertical up (i = row-1 because we dont want to check the current row)
+        for(int i = row-1; i>=0; i--) {
+            //going vertically up by just reducing row number
+            if(board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        // check diagonal left upward
+        for(int i = row-1, j = col-1; i>=0 && j>=0; i--, j--) {
+            // taking the path diagonally left up by reducing row and col.
+            if(board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        // check diagonally right upward - by decreasing row number and increasing column number
+        // try to visualize in head what am i trying to do here
+        for(int i = row-1, j = col+1; i>=0 && j<board.length; i--, j++) {
+            if(board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
     }
     
     public static void printBoard(char[][] board) {
